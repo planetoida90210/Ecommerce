52f4c316679fc2abbec723cbecdf67853df57b2a
planetoida90210 <galeriakoloru90210@gmail.com>
2021-12-09 01:09:04 +0100

    adding slider fn

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,7 +34,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
-    transform: translateX(0vw);
+    transition: all 0.3s ease;
+    transform: translateX(${props=>props.slideIndex * -100 }vw);
 `
 
 const Slide = styled.div`
@@ -76,6 +77,11 @@ const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0)
   const handleClick = (direction) => {
 
+    if(direction==="left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2)
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
   }
 
   return (
@@ -83,7 +89,7 @@ const Slider = () => {
         <Arrow direction="left" onClick={()=> handleClick("left")}>
           <ArrowLeftOutlined/>
         </Arrow>
-        <Wrapper>
+        <Wrapper slideIndex={slideIndex}>
           {sliderItems.map(item=>(
              <Slide bg={item.bg}>
              <ImgContainer>
