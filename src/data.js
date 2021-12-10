e21a65b4cad33b5be99c848178d849b5e0bd7a9f
planetoida90210 <galeriakoloru90210@gmail.com>
2021-12-10 22:48:48 +0100

    products boilerplate

--- src/data.js
+++ src/data.js
@@ -30,12 +30,47 @@ export const categories = [
   },
   {
     id: 2,
-    img: 'https://pictures.depop.com/b0/10580573/709846847_c57fb9b67d704ca39e17917616d9a084/P7.jpg',
+    img: 'https://i.pinimg.com/564x/64/5d/10/645d10e0db3cdbfb3ad233efaf2a5f67.jpg',
     title: "HANDMADE RUGS!"
   },
   {
     id: 3,
-    img: 'https://pictures.depop.com/b0/10580573/709846847_c57fb9b67d704ca39e17917616d9a084/P7.jpg',
+    img: 'https://i.pinimg.com/originals/0d/b2/24/0db224d359d14e855f8142e9805ec5d7.jpg',
     title: "CUSTOM RUGS!"
   },
 ]  
+ 
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://preview.redd.it/8h4hy5ocucg71.jpg?width=640&crop=smart&auto=webp&s=2f141f426ddb45a6fa965d57cfcff570bfa57602',
+  },
+  {
+    id: 2,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPVbCPfIutkiXULLezrDphSduPL0oqJnWIUtSnkSnsCZg_JNEat3bS4O0KwoafosPtYL8&usqp=CAU',
+  },
+  {
+    id: 3,
+    img: 'https://i.ytimg.com/vi/mb74j7dhZs4/maxresdefault.jpg',
+  },
+  {
+    id: 4,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLlVODOUNNG_W328GwyMXqXhYYzp0iXRqQVYd_ey-e8OTvOMqlVZtb-dp6UMEhFisbhag&usqp=CAU',
+  },
+  {
+    id: 5,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOkQ9tEO6mk69R1tT1GfCG_v4Y3tHH4GAqXoVNUJIYkKjkyTRqcBkLII9-y2elsDIsHWg&usqp=CAU',
+  },
+  {
+    id: 6,
+    img: 'http://sc04.alicdn.com/kf/H647467f60cf0407a8feae15984cda512z/Custom-Cartoon-Rugs-Decorative-Bedrooms-Custom-Carpets.jpg_350x350.jpg',
+  },
+  {
+    id: 7,
+    img: 'https://i.etsystatic.com/21896949/r/il/1e7df4/3336171648/il_fullxfull.3336171648_qhfa.jpg',
+  },
+  {
+    id: 8,
+    img: 'https://i.etsystatic.com/32747731/r/il/5b252a/3504916584/il_340x270.3504916584_gf6f.jpg',
+  }
+]
