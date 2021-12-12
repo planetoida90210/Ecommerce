167438e18bb84aee8cf6fcdb63256483c8e8cedf
planetoida90210 <galeriakoloru90210@gmail.com>
2021-12-12 15:52:55 +0100

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Product from './pages/Product'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 
 const App = () => {
- return <Register />
+ return <Login />
 }
 
 export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255,255,255,0.5),rgba(255,250,250, 0.5)), url("https://i.ytimg.com/vi/mb74j7dhZs4/maxresdefault.jpg") center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+  color: ${props=>props.color};
+`
+
+const Login = () => {
+  return (
+    <Container>
+    <Wrapper>
+      <Title>SIGN IN</Title>
+      <Form>
+        <Input placeholder='username' />
+        <Input placeholder='password' />
+        <Button>LOGIN</Button>
+        <Link color="coral">FORGOT THE PASSWORD?</Link>
+        <Link color="gray">CREATE A NEW ACCOUNT</Link>
+      </Form>
+    </Wrapper>
+  </Container>
+  )
+}
+
+export default Login
