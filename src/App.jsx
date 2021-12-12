8bbfaf809191481dfc3e23b4b074b42ce3309771
planetoida90210 <galeriakoloru90210@gmail.com>
2021-12-12 15:38:00 +0100

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 import Home from './pages/Home'
 import Product from './pages/Product'
 import ProductList from './pages/ProductList'
+import Register from './pages/Register'
 
 const App = () => {
- return <Product />
+ return <Register />
 }
 
 export default App
